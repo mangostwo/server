067fb5bcbe3978202b56aaeb3d5a09e81b0abe79
Charles A Edwards <charles.a.edwards@btinternet.com>
2015-03-25 10:57:57 +0000

    Case statement not terminated properly
    missing break;
    
    It caused a start-up error, besides being totally wrong.
    
    My bad :(

--- src/game/Object/ObjectMgr.cpp
+++ src/game/Object/ObjectMgr.cpp
@@ -8830,7 +8830,8 @@ bool PlayerCondition::IsValid(uint16 entry, ConditionType condition, uint32 valu
             {
                 sLog.outErrorDb("Creature in range condition (entry %u, type %u) has an invalid value in value2. (Range %u must be greater than 0), skipping.", entry, condition, value2);
                 return false;
-            }
+			}
+			break;
         }
         case CONDITION_GAMEOBJECT_IN_RANGE:
         {
@@ -8843,7 +8844,8 @@ bool PlayerCondition::IsValid(uint16 entry, ConditionType condition, uint32 valu
             {
                 sLog.outErrorDb("Game object in range condition (entry %u, type %u) has an invalid value in value2 (range). (Range %u must be greater than 0), skipping.", entry, condition, value2);
                 return false;
-            }
+			}
+			break;
         }
         case CONDITION_NONE:
             break;
