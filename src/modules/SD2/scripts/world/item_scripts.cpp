99bcd2dfcfc03fc55eb4039c99c2b26eef83b442
Charles A Edwards <charles.a.edwards@btinternet.com>
2015-05-21 07:02:20 +0100

    SylvainNeau (author) - Ogre Brew script
    This makes the potion only usable in Blades Edge

--- src/modules/SD2/scripts/world/item_scripts.cpp
+++ src/modules/SD2/scripts/world/item_scripts.cpp
@@ -23,21 +23,64 @@
  * and lore are copyrighted by Blizzard Entertainment, Inc.
  */
 
-/* ScriptData
-SDName: Item_Scripts
-SD%Complete: 100
-SDComment: Items for a range of different items. See content below (in script)
-SDCategory: Items
-EndScriptData */
+/**
+ * ScriptData
+ * SDName:      Item_Scripts
+ * SD%Complete: 100
+ * SDComment: Items for a range of different items. See content below (in script)
+ * SDCategory:  Items
+ * EndScriptData
+ */
 
-/* ContentData
-item_arcane_charges                 Prevent use if player is not flying (cannot cast while on ground)
-item_flying_machine(i34060,i34061)  Engineering crafted flying machines
-item_gor_dreks_ointment(i30175)     Protecting Our Own(q10488)
-EndContentData */
+/**
+ * ContentData
+ * item_arcane_charges                 Prevent use if player is not flying (cannot cast while on ground)
+ * item_flying_machine(i34060,i34061)  Engineering crafted flying machines
+ * item_gor_dreks_ointment(i30175)     Protecting Our Own(q10488)
+ * EndContentData
+ */
 
 #include "precompiled.h"
 #include "Spell.h"
+
+/*#####
+# item_ogre_brew
+#####*/
+
+enum
+{
+    BLUE_OGRE_BREW	= 32783,
+    RED_OGRE_BREW	= 32784,
+    TRANCHANTES		= 3522,
+    OGRE_BREW_MANA	= 41304,
+    OGRE_BREW_LIFE	= 41306
+};
+
+bool ItemUse_item_ogre_brew(Player* pPlayer, Item* pItem, const SpellCastTargets&)
+{
+    uint32 itemId	= pItem->GetEntry();
+    uint32 pZoneId	= pPlayer->GetZoneId();
+    
+    if (itemId == BLUE_OGRE_BREW || itemId == RED_OGRE_BREW)
+    {
+        if (pZoneId == TRANCHANTES)
+        {
+            return false;
+        }
+        debug_log("SD2: Player attempt to use item %u, but did not meet zone requirement : %u", itemId,pZoneId);
+        if (itemId == BLUE_OGRE_BREW)
+        {
+            const SpellEntry* pSpellInfo = GetSpellStore()->LookupEntry(OGRE_BREW_LIFE);
+            Spell::SendCastResult(pPlayer, pSpellInfo, 1, SPELL_FAILED_NOT_HERE);
+        }
+        if (itemId == RED_OGRE_BREW)
+        {
+            const SpellEntry* pSpellInfo = GetSpellStore()->LookupEntry(OGRE_BREW_MANA);
+            Spell::SendCastResult(pPlayer, pSpellInfo, 1, SPELL_FAILED_NOT_HERE);
+        }
+        return true;
+    }
+}
 
 /*#####
 # item_arcane_charges
@@ -56,7 +99,7 @@ bool ItemUse_item_arcane_charges(Player* pPlayer, Item* pItem, const SpellCastTa
     pPlayer->SendEquipError(EQUIP_ERR_NONE, pItem, NULL);
 
     if (const SpellEntry* pSpellInfo = GetSpellStore()->LookupEntry(SPELL_ARCANE_CHARGES))
-    { Spell::SendCastResult(pPlayer, pSpellInfo, 1, SPELL_FAILED_NOT_ON_GROUND); }
+    { Spell::SendCastResult(pPlayer, pSpellInfo, 1, SPELL_FAILED_ERROR); }
 
     return true;
 }
@@ -107,38 +150,14 @@ bool ItemUse_item_gor_dreks_ointment(Player* pPlayer, Item* pItem, const SpellCa
     return false;
 }
 
-/*#####
-# item_petrov_cluster_bombs
-#####*/
-
-enum
-{
-    SPELL_PETROV_BOMB           = 42406,
-    AREA_ID_SHATTERED_STRAITS   = 4064,
-    ZONE_ID_HOWLING             = 495
-};
-
-bool ItemUse_item_petrov_cluster_bombs(Player* pPlayer, Item* pItem, const SpellCastTargets& /*pTargets*/)
-{
-    if (pPlayer->GetZoneId() != ZONE_ID_HOWLING)
-        return false;
-
-    if (!pPlayer->GetTransport() || pPlayer->GetAreaId() != AREA_ID_SHATTERED_STRAITS)
-    {
-        pPlayer->SendEquipError(EQUIP_ERR_NONE, pItem, NULL);
-
-        if (const SpellEntry* pSpellInfo = GetSpellStore()->LookupEntry(SPELL_PETROV_BOMB))
-            Spell::SendCastResult(pPlayer, pSpellInfo, 1, SPELL_FAILED_NOT_HERE);
-
-        return true;
-    }
-
-    return false;
-}
-
 void AddSC_item_scripts()
 {
     Script* pNewScript;
+    
+    pNewScript = new Script;
+    pNewScript->Name = "item_ogre_brew";
+    pNewScript->pItemUse = &ItemUse_item_ogre_brew;
+    pNewScript->RegisterSelf(); 
 
     pNewScript = new Script;
     pNewScript->Name = "item_arcane_charges";
@@ -153,10 +172,5 @@ void AddSC_item_scripts()
     pNewScript = new Script;
     pNewScript->Name = "item_gor_dreks_ointment";
     pNewScript->pItemUse = &ItemUse_item_gor_dreks_ointment;
-    pNewScript->RegisterSelf();
-
-    pNewScript = new Script;
-    pNewScript->Name = "item_petrov_cluster_bombs";
-    pNewScript->pItemUse = &ItemUse_item_petrov_cluster_bombs;
     pNewScript->RegisterSelf();
 }
