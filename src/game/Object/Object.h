91e82fb68d4df99056c6b838050c4a54ee4d9485
Antz <antz@getmangos.eu>
2016-12-07 20:24:53 +0000

    remove obsolete code. Thanks H0zen

--- src/game/Object/Object.h
+++ src/game/Object/Object.h
@@ -183,7 +183,6 @@ class Object
 
         void BuildValuesUpdateBlockForPlayer(UpdateData* data, Player* target) const;
         void BuildOutOfRangeUpdateBlock(UpdateData* data) const;
-        void BuildMovementUpdateBlock(UpdateData* data, uint16 flags = 0) const;
 
         virtual void DestroyForPlayer(Player* target, bool anim = false) const;
 
