3cecbb851fc953a51352ec5a69827e21ebc807ec
Necrovoice <dragonsfrost@hotmail.com>
2016-12-23 21:46:38 -0500

    Rogue Stealth corrections
    A few corrections to the rogue stealth.

--- src/game/Object/AggressorAI.cpp
+++ src/game/Object/AggressorAI.cpp
@@ -6,7 +6,7 @@
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
+ * the Free Software Foundation; either version 2 of the License, orRemoveSpellsCausingAura
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
@@ -63,8 +63,7 @@ AggressorAI::MoveInLineOfSight(Unit* u)
             if (!m_creature->getVictim())
             {
                 AttackStart(u);
-                u->RemoveSpellsCausingAura(SPELL_AURA_MOD_STEALTH);
-            }
+                            }
             else if (sMapStore.LookupEntry(m_creature->GetMapId())->IsDungeon())
             {
                 m_creature->AddThreat(u);
