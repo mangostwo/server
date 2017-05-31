e90651dbfcf94711d4907e22093dcb5c4243d6ca
Antz <antz@getmangos.eu>
2017-05-31 21:55:53 +0100

    [DB] Update base world DB to Rel21.06.047

--- src/shared/revision.h
+++ src/shared/revision.h
@@ -24,7 +24,7 @@
 
 #ifndef MANGOS_H_REVISION
 #define MANGOS_H_REVISION
-    #define REVISION_NR "2106009"
+    #define REVISION_NR "2106047"
 
     #define REALMD_DB_VERSION_NR 21
     #define REALMD_DB_STRUCTURE_NR 1
@@ -38,6 +38,6 @@
 
     #define WORLD_DB_VERSION_NR 21
     #define WORLD_DB_STRUCTURE_NR 6
-    #define WORLD_DB_CONTENT_NR 9
-    #define WORLD_DB_UPDATE_DESCRIPTION "Gossip_texts_updates"
+    #define WORLD_DB_CONTENT_NR 47
+    #define WORLD_DB_UPDATE_DESCRIPTION "q9294_Healing_the_Lake"
 #endif // __REVISION_H__
