f4e060b99a04549401be62a6c403e1806b6d50f6
Antz <antz@getmangos.eu>
2018-01-05 05:37:24 +0000

    [Eluna] Add conditionals around code

--- src/game/WorldHandlers/GameEventMgr.cpp
+++ src/game/WorldHandlers/GameEventMgr.cpp
@@ -35,7 +35,10 @@
 #include "MassMailMgr.h"
 #include "SpellMgr.h"
 #include "Policies/Singleton.h"
+
+#ifdef ENABLE_ELUNA
 #include "LuaEngine.h"
+#endif /* ENABLE_ELUNA */
 
 INSTANTIATE_SINGLETON_1(GameEventMgr);
 
@@ -84,8 +87,10 @@ void GameEventMgr::StartEvent(uint16 event_id, bool overwrite /*=false*/, bool r
         if (mGameEvent[event_id].end <= mGameEvent[event_id].start)
             { mGameEvent[event_id].end = mGameEvent[event_id].start + mGameEvent[event_id].length; }
     }
+#ifdef ENABLE_ELUNA
     if (IsActiveEvent(event_id))
         sEluna->OnGameEventStart(event_id);
+#endif /* ENABLE_ELUNA */
 }
 
 void GameEventMgr::StopEvent(uint16 event_id, bool overwrite)
@@ -97,8 +102,10 @@ void GameEventMgr::StopEvent(uint16 event_id, bool overwrite)
         if (mGameEvent[event_id].end <= mGameEvent[event_id].start)
             { mGameEvent[event_id].end = mGameEvent[event_id].start + mGameEvent[event_id].length; }
     }
+#ifdef ENABLE_ELUNA
     if (!IsActiveEvent(event_id))
         sEluna->OnGameEventStop(event_id);
+#endif /* ENABLE_ELUNA */
 }
 
 void GameEventMgr::LoadFromDB()
