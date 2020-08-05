818e5e4b1d10b429d1f3fe8a85fe7d34133da01e
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2020-08-05 10:15:11 +0500

    Fixed(Linux, MMaps extracting): wrong path to MoveMapsGen.sh

--- linux/getmangos.sh
+++ linux/getmangos.sh
@@ -1421,9 +1421,9 @@ function ExtractResources
 
         Log "Copying MMaps extractor" 0
         rm -f "$GAMEPATH/MoveMapGen.sh"
-        cp "$INSTPATH/bin/tools/MoveMapGen.sh" "$GAMEPATH"
-        cp "$INSTPATH/bin/tools/offmesh.txt" "$GAMEPATH"
-        cp "$INSTPATH/bin/tools/mmap_excluded.txt" "$GAMEPATH"
+        cp "$INSTPATH/bin/MoveMapGen.sh" "$GAMEPATH"
+        cp "$INSTPATH/bin/offmesh.txt" "$GAMEPATH"
+        cp "$INSTPATH/bin/mmap_excluded.txt" "$GAMEPATH"
         cp "$INSTPATH/bin/tools/mmap-extractor" "$GAMEPATH"
 
         CPU=$($DLGAPP --backtitle "MaNGOS Linux Build Configuration" --title "Please provide the number of CPU to be used to generate MMaps (1-4)" \
