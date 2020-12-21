68943eb1061d473fbc51cab12c93b68253aac3ef
Antz <billy1arm@users.noreply.github.com>
2020-12-21 23:32:51 +0000

    [Realm] fix account table errors

--- src/shared/revision.h
+++ src/shared/revision.h
@@ -27,9 +27,9 @@
     #define REVISION_NR "2113001"
 
     #define REALMD_DB_VERSION_NR 21
-    #define REALMD_DB_STRUCTURE_NR 3
+    #define REALMD_DB_STRUCTURE_NR 4
     #define REALMD_DB_CONTENT_NR 001
-    #define REALMD_DB_UPDATE_DESCRIPTION "Update Row format and engine"
+    #define REALMD_DB_UPDATE_DESCRIPTION "fix account table errors"
 
     #define CHAR_DB_VERSION_NR 21
     #define CHAR_DB_STRUCTURE_NR 4
