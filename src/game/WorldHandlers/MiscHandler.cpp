138e3b39a09264008a5e26a7215a347231b6d9f1
Tristan Cormier <cormiert2@outlook.com>
2021-01-15 20:13:30 -0500

    Implemented the 'level' client command handler

--- src/game/WorldHandlers/MiscHandler.cpp
+++ src/game/WorldHandlers/MiscHandler.cpp
@@ -1360,6 +1360,36 @@ void WorldSession::GmResurrectHandler(WorldPacket &msg)
     }
 }
 
+/****************************************/
+/* This function handles the 'level' client command. */
+/****************************************/
+void WorldSession::LevelCheatHandler(WorldPacket& msg)
+{
+    DEBUG_LOG("WORLD: Received %s message from account %d:", msg.GetOpcodeName(), GetAccountId());
+
+    /* Check that we have permission to perform the function */
+    if (GetSecurity() > SEC_PLAYER)
+    {
+        uint32 curLevel = GetPlayer()->getLevel();
+        uint32 newLevel = 0;
+
+        msg >> newLevel;
+
+        /* Check that the level value is greater than 0 and smaller than the server's max player level for its expansion level */
+        /* and that we have a different level than the one requested... */
+        if ((newLevel != 0 && newLevel <= DEFAULT_MAX_LEVEL) && newLevel != curLevel)
+        {
+            DEBUG_LOG("Leveling player %s from level %d to %d", GetPlayerName(), curLevel, newLevel);
+            GetPlayer()->SetLevel(newLevel);
+        }
+    }
+    else
+    {
+        DEBUG_LOG("Permission denied.");
+        SendNotification(LANG_YOU_NOT_HAVE_PERMISSION);
+    }
+}
+
 void WorldSession::HandleWhoisOpcode(WorldPacket& recv_data)
 {
     DEBUG_LOG("WORLD: Received opcode CMSG_WHOIS");
